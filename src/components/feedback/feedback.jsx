0e7a2c1e8abd28a82a2467f25e2e88b7243eee86
ololosh-tralevich <karpeko76@gmail.com>
2022-02-10 19:24:55 +0200

    porn with first task

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,25 +1,52 @@
-import styles from './feedback.module.css';
+import { Component } from 'react';
 
-const Feedback = () => {
-  return (
-    <section className={styles.mainContainer}>
-      <div className={styles.feedbackBlock}>
-        <h2>Please leave feedback</h2>
-        <button className={styles.ratingButton}>Good</button>
-        <button className={styles.ratingButton}>Neutral</button>
-        <button className={styles.ratingButton}>Bad</button>
-      </div>
-      <div>
-        <h3>STATISTICS</h3>
-      </div>
-    </section>
-  );
-};
+import styles from './feedback.module.css';
+// import PropTypes from "prop-types";
+
+class Feedback extends Component {
+  state = {
+    good: 3,
+    neutral: 1,
+    bad: 13,
+  };
+
+  render() {
+    return (
+      <section className={styles.mainContainer}>
+        <div className={styles.feedbackBlock}>
+          <h2>Please leave feedback</h2>
+          <button
+            className={styles.ratingButton}
+            onClick={evt => (this.state.good += 1)}
+          >
+            Good
+          </button>
+          <button className={styles.ratingButton}>Neutral</button>
+          <button className={styles.ratingButton}>Bad</button>
+        </div>
+        <div className={styles.statBlock}>
+          <h3>STATISTICS</h3>
+          <div className={styles.ratingBlock}>
+            <p>Good: {this.state.good}</p>
+            <p>Neutral: {this.state.neutral}</p>
+            <p>Bad: {this.state.bad}</p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+// FriendList.defaultProps = {
+//   friends: [],
+// };
+
+// Feedback.PropTypes = {
+//   state: PropTypes.arrayOf(
+//    PropTypes.shape({
+//     good: PropTypes.number.isRequired,
+//     neutral: PropTypes.number.isRequired,
+//     bad: PropTypes.number.isRequired,}
+//   ))
+// }
 
 export default Feedback;
-
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0
-// }
